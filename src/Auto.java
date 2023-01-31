b8191974ec1225c84673c6e317eb2f0d70c58522
qmar <video4den@gmail.com>
2023-01-31 20:38:09 +0500

    сделал задание 1

--- src/Auto.java
+++ src/Auto.java
@@ -1,2 +1,24 @@
 public class Auto {
+    String brand;
+    String model;
+    double engineVolume;
+    String color;
+    int year;
+    String country;
+
+
+
+
+    @Override
+    public String toString() {
+        return "Auto" +
+                "\n марка: " + brand +
+                "\n модель: " + model +
+                "\n engineVolume: " + engineVolume +
+                "\n color: " + color +
+                "\n year: " + year +
+                "\n country: " + country;
+    }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+        Auto lada = new Auto();
+        lada.brand = "Lada";
+        lada.model = "Granta";
+        lada.engineVolume = 1.7;
+        lada.color = "жёлтый";
+        lada.year = 2005;
+        lada.country = "Россия";
+
+        Auto audi = new Auto();
+        audi.brand = "Audi";
+        audi.model = "A8 50 L TDI quattro";
+        audi.engineVolume = 3.0;
+        audi.color = "чёрный";
+        audi.year = 2020;
+        audi.country = "Германия";
+
+        Auto bmw = new Auto();
+        bmw.brand = "BMW";
+        bmw.model = "Z8";
+        bmw.engineVolume = 3.0;
+        bmw.color = "чёрный";
+        bmw.year = 2021;
+        bmw.country = "Германия";
+
+        Auto kia = new Auto();
+        kia.brand = "Kia";
+        kia.model = "Sportage 4-го поколения";
+        kia.engineVolume = 2.4;
+        kia.color = "красный";
+        kia.year = 2018;
+        kia.country = "Южная Корея";
+
+        Auto hyundai = new Auto();
+        hyundai.brand = "Hyundai";
+        hyundai.model = "Avante";
+        hyundai.engineVolume = 1.6;
+        hyundai.color = "оранжевый";
+        hyundai.year = 2016;
+        hyundai.country = "Южная Корея";
+
+        System.out.println(lada.toString());
+        System.out.println(audi.toString());
+        System.out.println(bmw.toString());
+        System.out.println(kia.toString());
+        System.out.println(hyundai.toString());
+
     }
+
 }
