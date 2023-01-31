f20c8ef558ee1d9b33c3b565b396bd41fd913b65
qmar <video4den@gmail.com>
2023-01-31 23:21:04 +0500

    сделал задание 3

--- src/Auto.java
+++ src/Auto.java
@@ -9,13 +9,14 @@ public class Auto {
 
 
     public Auto(String brand, String model, double engineVolume, String color, int year, String country) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.year = year;
-        this.country = country;
+        this.brand = (brand == null || brand == "" ? "default" : brand);
+        this.model = (model == null || model == "" ? "default" : model);
+        this.engineVolume = (engineVolume <= 0 ? 1.5 : engineVolume);
+        this.color = (color == null || color == "" ? "белый" : color);
+        this.year = (year <= 0 ? 2000 : year);
+        this.country = (country == null || country == "" ? "default" : country);
     }
+
     @Override
     public String toString() {
         return "Auto" +
